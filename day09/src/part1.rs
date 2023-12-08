456bd03419535aca987052dfa68853a64696aa14
Janusz Piechówka <januszpiechowka@gmail.com>
2023-12-08 22:10:57 +0100

    chore: prepare for day 9

---
+++ day09/src/part1.rs
@@ -0,0 +1,27 @@
+use std::time::Instant;
+
+mod parser;
+
+fn main() {
+    let start_time = Instant::now();
+    let input_file = include_str!("../input_p1.txt");
+    let solution = part1(input_file);
+    let execution_time = start_time.elapsed().as_secs_f64();
+    println!("[{execution_time:?} seconds] {solution}");
+}
+
+fn part1(input: &str) -> u64 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn d09p1_full_input() {
+        let input = "";
+
+        assert_eq!(part1(input), 0);
+    }
+}
